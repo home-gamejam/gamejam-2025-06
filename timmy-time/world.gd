2a39f1b7881bcd1679fe440ee3cdcf5b14095b45
Gage Bovi <gage.bovi@gmail.com>
2025-06-28 15:43:31 -0500

    its done mostly

---
+++ timmy-time/world.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+func _on_telapotty_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://ground_land.tscn")
+	
