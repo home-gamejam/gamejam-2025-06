bc1e6ed6dfe1aca536980f1623f4805ddc1a96d8
Judah <judah.ingles@outlook.com>
2025-06-28 14:33:12 -0500

    funny spongebob

--- timmy-time/node_2d.gd
+++ timmy-time/node_2d.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
+@onready var animations = $AnimationPlayer
+
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
+func _update_animations():
+	if velocity.x < 0:
+		$Sprite2D.flip_h = true
+	elif velocity.x > 0:
+		$Sprite2D.flip_h = false
+		
+
 
 func _physics_process(delta: float) -> void:
+	#if Input.is_action_pressed("E"):
+		#animations.play("keytar")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,7 +30,18 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_pressed("E"):
+			animations.play("keytar")
+		else:
+			animations.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if Input.is_action_pressed("E"):
+			animations.play("keytar")
+		else:
+			$AudioStreamPlayer2D.play()
+			animations.play("RESET")
+	
 
 	move_and_slide()
+	_update_animations()
